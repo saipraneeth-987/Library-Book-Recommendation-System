49666f51bc782ab706a33fc9032d2e28a0a86fe2
Pragnya <112101029@smail.iitpkd.ac.in>
2024-12-18 03:25:27 +0530

    Added a description part for todo

--- todo/todo.py
+++ todo/todo.py
@@ -1,13 +1,14 @@
 from fasthtml.common import *
 
 # Initialize the app, routes, and the Todo model with the database
-app, rt,todos, Todo = fast_app('data/todos.db', id=int, title=str, done=bool, pk='id')
+app, rt,todos, Todo = fast_app('data/todos.db', id=int, title=str, description=str, done=bool, pk='id')
 
 # Function to display a single Todo item in the list
 def TodoRow(todo):
     return Li(
         H6(todo.title, style="display: inline; "),
         A(' edit', href=f'/edit/{todo.id}', style="display: inline; margin-left: 15px"),
+        Span(todo.description, style="font-size: smaller; display: block; margin-top: 5px;"),
         id=f'todo-{todo.id}'
     )
 
@@ -33,11 +34,16 @@ def home():
     # Form to add a new Todo
     add = Form(
         Group(
-            Input(name="title", placeholder="New Todo"),
-            Button("Add")
-        ), action="/", method='post'
+            Group(
+                Input(name="title", placeholder="New Todo",required = True),
+                Input(name="description", placeholder="Add description",style="font-size:smaller"),
+                style="display: flex; flex-direction: column; align-items: flex-start; gap: 10px;"
+            ),
+            Button("Add", style="align-self: center; margin-left:15px ;"),
+            style="display: flex; justify-content: space-between; align-items: center; width: 98%;"
+        ),
+        action="/", method='post'
     )
-
     # Create a list of active Todos (not done)
     active_todos = Ul(*map(TodoRow, filter(is_active, todos())), id='active-todo-list')
 
@@ -79,15 +85,22 @@ def remove_todo(id: int):
 @app.get("/edit/{id}")
 def edit_todo(id: int):
     res = Form(
+        Button("Save",role="button", style="margin-bottom: 15px;"),
         Group(
+            H6("Title", style="margin-right: 10px; min-width: 60px; text-align: left;"),
             Input(id="title"),
-            Button("Save")
-        ),
-        Hidden(id="id"),
+            style="display: flex; align-items: center; gap: 10px;"
+            ),
+        Group(
+            H6("Description", style="margin-right: 10px; min-width: 60px;white-space: nowrap; text-align: left;"),
+            Input(id="description", placeholder="Add Description"),
+            style="display: flex; align-items: center; gap: 10px;"
+            ),
         CheckboxX(id="done", label='done'),
-        A('Delete',href=f'/remove?id={id}',role="button"),
+        Hidden(id="id"),
+        A('Delete',href=f'/remove?id={id}',role="button",onclick="return confirm('Are you sure you want to delete this Todo?');"),
         A('Back', href='/', role="button",style="margin:15px"),action="/update", id="edit", method='post'
-        )
+    )
     frm = fill_form(res, todos[id])
     return Titled('Edit Todo', frm)
 
@@ -98,9 +111,9 @@ def edit_todo_done(id: int):
             Button("Save",style = "margin-bottom:10px"),
         Hidden(id="id"),
         CheckboxX(id="done", label='not done' ),  # Uncheck to mark as undone
-        A('Delete',href=f'/remove?id={id}',role="button"),
-        A('Back', href='/', role="button",style="margin:15px"),action="/update", id="edit", method='post'
+        A('Delete',href=f'/remove?id={id}',role="button",onclick="return confirm('Are you sure you want to delete this Todo?');"),
 
+        A('Back', href='/', role="button",style="margin:15px"),action="/update", id="edit", method='post'
     )
     frm = fill_form(res, todos[id])
     return Titled('Edit Tododone', frm)

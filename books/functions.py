f9d96414e3758687860aa69937091a914dd2c489
Pragnya <112101029@smail.iitpkd.ac.in>
2025-01-17 21:46:03 +0530

    Minor modificiation

--- books/functions.py
+++ books/functions.py
@@ -20,22 +20,25 @@ def update_stage(isbn: int, current_stage: int,new_stage: int):
         connection.commit()
 
 def get_book_details(isbn):
-    api_key = "AIzaSyBVqOwuDKlY35_CSFSuhWzcAP4MIGnqLLU"
-    url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={api_key}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        print(data)
-        if "items" in data:
-            book = data["items"][0]["volumeInfo"]
-            title = book.get("title", "Unknown Title") + ": " + book.get("subtitle")
-            authors = ", ".join(book.get("authors", ["Unknown Author"]))
-            publishers = book.get("publisher", ["Unknown Publisher"])
-            return {"title": title, "authors": authors, "publisher":publishers}
+    if(isbn):
+        api_key = "AIzaSyBVqOwuDKlY35_CSFSuhWzcAP4MIGnqLLU"
+        url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}&key={api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            print(data)
+            if "items" in data:
+                book = data["items"][0]["volumeInfo"]
+                title = book.get("title", "Unknown Title") + ": " + book.get("subtitle")
+                authors = ", ".join(book.get("authors", ["Unknown Author"]))
+                publishers = book.get("publisher", ["Unknown Publisher"])
+                return {"title": title, "authors": authors, "publisher":publishers}
+            else:
+                return {"error": "Book not found"}
         else:
-            return {"error": "Book not found"}
+            return {f"Failed to fetch details: {response.status_code}"}
     else:
-        return {f"Failed to fetch details: {response.status_code}"}
+        return {""}
 
 
 def filter_by_date(all_items, date_range):

584ef4ebaa4d86ea68dd519f8eeb5f134a5ba23f
Pragnya <112101029@smail.iitpkd.ac.in>
2025-01-17 21:41:20 +0530

    Added download for all stages

--- books/download.py
+++ books/download.py
@@ -12,7 +12,7 @@ def download_whole():
     writer = csv.writer(csv_file)
 
     # Write the header row for the CSV file
-    writer.writerow(["ID", "ISBN", "Recommender", "Email", "Number of Copies", "Purpose", "Remarks", "Date","status","modified_isbn","Title","Sub_Title","publisher","Edition/Year","author","currency","cost_currency","cost_inr","total_cost","approval_remarks","seller","current_stage","Recent Action Date","availability in Processing","Remarks while Processing","Remarks_Processing","Availability_Enquiry","Supplier Information","Remarks_Enquiry"])
+    writer.writerow(["ID", "ISBN", "Recommender", "Email", "Number of Copies", "Purpose", "Remarks", "Date","status","modified_isbn","Title","Sub_Title","publisher","Edition/Year","author","currency","cost_currency","cost_inr","total_cost","approval_remarks","seller","current_stage","Recent Action Date","availability in Processing","Remarks while Processing","Remarks_Processing","Availability_Enquiry","Supplier Information","Remarks_Enquiry","Remarks after Ordered","Remarks After Received"])
 
     # Connect to the SQLite database and fetch all items
     connection = sqlite3.connect('data/library.db')
@@ -293,4 +293,34 @@ def download_stage6():
         csv_file,
         media_type="text/csv",
         headers={"Content-Disposition": "attachment; filename=books_Ordered.csv"}
+    )
+
+def download_stage7():
+    csv_file = StringIO()
+    writer = csv.writer(csv_file)
+
+    # Write the header row for the CSV file
+    writer.writerow(["ID","ISBN", "Title", "Sub Title", "Author","Publisher","Edition/year","Number of copies","Currency", "Recommender","Purpose of recommendation","Cost in Currency","Status","Approval Remarks","Book Availability","supplier Information","Remarks while Enquiry","Remarks while Ordering","Remarks After Received", "Recent Action Date"])
+
+    # Connect to the SQLite database and fetch all items
+    connection = sqlite3.connect('data/library.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT id,modified_isbn,book_name,sub_title,authors,publisher,edition_or_year, number_of_copies, currency,recommender,purpose,cost_currency,status,approval_remarks,availability_stage5,supplier_info,remarks_stage5,remarks_stage6,remarks_stage7,date_stage_update FROM items WHERE current_stage = 7 ")
+    items = cursor.fetchall()
+
+    # Write each item to the CSV file
+    for item in items:
+        writer.writerow(item)
+
+    # Close the database connection
+    connection.close()
+
+    # Reset the cursor to the beginning of the buffer
+    csv_file.seek(0)
+
+    # Return the CSV file as a streaming response for download
+    return StreamingResponse(
+        csv_file,
+        media_type="text/csv",
+        headers={"Content-Disposition": "attachment; filename=books_Received.csv"}
     )

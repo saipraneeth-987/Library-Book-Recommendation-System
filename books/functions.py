88870328fc120198b05b157dc68aabe3fc95964f
Pragnya <112101029@smail.iitpkd.ac.in>
2025-01-18 09:33:41 +0530

    Minor edit

--- books/functions.py
+++ books/functions.py
@@ -29,10 +29,11 @@ def get_book_details(isbn):
             print(data)
             if "items" in data:
                 book = data["items"][0]["volumeInfo"]
-                title = book.get("title", "Unknown Title") + ": " + book.get("subtitle")
+                title = book.get("title", "Unknown Title") 
+                subtitle = book.get("subtitle", "")
                 authors = ", ".join(book.get("authors", ["Unknown Author"]))
                 publishers = book.get("publisher", ["Unknown Publisher"])
-                return {"title": title, "authors": authors, "publisher":publishers}
+                return {"title": title, "subtitle":subtitle,"authors": authors, "publisher":publishers}
             else:
                 return {"error": "Book not found"}
         else:
